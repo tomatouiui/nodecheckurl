1782313dbe0031e724e70c7549a8401c80e4d508
Toma <yuzhaok@cn.ibm.com>
2020-08-12 10:28:59 +0800

    b

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const urlStatusCode = require('url-status-code')
 const url = 'https://www.ibm.com/cn/zh'
 
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'savefrom_db'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'savefrom_db'
 });
- 
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
- 
-urlStatusCode(url, (error, statusCode) => {
-  if (error) {
-    console.error(error)
-  } else {
-    console.log(statusCode)
-  }
-})
 
+connection.connect();
+
+connection.query('SELECT element_link from report_broken where id < 100', function(error, results, fields) {
+    if (error) throw error;
+    for (var i = 0; i < results.length; i++) {
+        //console.log('The url is: ', results[i].element_link);
+        urlStatusCode(results[i].element_link, (error, statusCode) => {
+            if (error) {
+                console.error(error)
+            } else {
+                console.log(statusCode)
+            }
+        })
+    }
+});
