0e1e9fa50800beda01ddef6d9e123bff4c6de1d7
Toma <yuzhaok@cn.ibm.com>
2020-08-18 12:40:42 +0800

    v

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var mysql = require('mysql');
 const fs = require('fs');
 var now = 0;
 
-fs.writeFile('errorlog.txt', '', function(err){
-    if(err) throw err;
+fs.writeFile('errorlog.txt', '', function(err) {
+    if (err) throw err;
     console.log('errorlog.txt已存在，内容被覆盖！');
 });
 
-fs.writeFile('tt.txt', '', function(err){
-    if(err) throw err;
+fs.writeFile('tt.txt', '', function(err) {
+    if (err) throw err;
     console.log('tt.txt已存在，内容被覆盖！');
 });
 
@@ -22,25 +22,27 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT element_link from report_broken where id < 1000', function(error, results, fields) {
+connection.query('SELECT element_link from report_broken where id < 10', function(error, results, fields) {
     if (error) throw error;
     for (var i = 0; i < results.length; i++) {
         //console.log('The url is ' + i + ' : ', results[i].element_link);
         var nowurl = results[i].element_link;
-
-        request(nowurl, function(error, response, data) {
-            if (error) {
-                console.log(error);
-                fs.appendFileSync('errorlog.txt', '\n'+error+'\n');
-            } else {
-              now++
-              console.log('now', now)
-                //console.log(response.request)
-                console.log(response.request.href)
-                //console.log('url', response.headers['x-acquia-host'] + response.headers['x-acquia-path'])
-                console.log('statusCode', response.statusCode)
-                fs.appendFileSync('tt.txt', '\n'+response.request.href+':'+response.statusCode);
-            }
-        });
+        checkurl(nowurl)
     }
 });
+
+function checkurl(nowurl) {
+    request(nowurl, function(error, response, data) {
+        if (error) {
+            console.log(error);
+            fs.appendFileSync('errorlog.txt', '\n' + error + '\n');
+        } else {
+            console.log('nowurl', nowurl)
+            //console.log(response.request)
+            //console.log(response.request.href)
+            //console.log('url', response.headers['x-acquia-host'] + response.headers['x-acquia-path'])
+            console.log('statusCode', response.statusCode)
+            fs.appendFileSync('tt.txt', '\n' + nowurl + ':' + response.statusCode);
+        }
+    });
+}
