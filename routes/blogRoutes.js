7e5690db845e8b0d7047fb46ced11cbc0c2730b8
尾巴 Toma <tomatouiui@gmail.com>
2020-09-10 09:20:08 +0000

    aaff

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/blogs/create', blogController.blog_create_get);
 router.get('/', blogController.blog_index);
-router.post('/', blogController.blog_create_post);
+router.post('/blog', blogController.blog_create_post);
 router.get('/blogs/:id', blogController.blog_details);
 router.delete('/blogs/:id', blogController.blog_delete);
 
